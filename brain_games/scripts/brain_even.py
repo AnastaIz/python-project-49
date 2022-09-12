2746919fad3302125c3c18ae0a358f4c0a623502
Anastasia Izmaylova <abulatova1992@gmail.com>
2022-09-12 17:24:03 +0300

    fix login in brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,25 +1,17 @@
-from random import randint
+import random
 from brain_games.scripts.game_engine import engine
-
-
-def is_even(number):
-    counter = 0
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            counter += 1
-    return (counter == 0 and number > 1) and 'yes' or 'no'
-
-
-def is_even_game():
-    number = randint(1, 20)
-    return number, is_even(number)
 
 
 game_question = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even():
+    number = random.randint(1, 100)
+    return number, number % 2 == 0 and 'yes' or 'no'
+
+
 def main():
-    engine(is_even_game, game_question)
+    engine(is_even, game_question)
 
 
 if __name__ == '__main__':
