a0f53f47e6b65ef7ca2a7aae631f86d4ff9f602f
Anastasia Izmaylova <abulatova1992@gmail.com>
2022-09-20 19:26:31 +0300

    modified game_engine for passing a module to engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -2,15 +2,14 @@ import prompt
 from brain_games.scripts.brain_games import greeting
 
 
-def engine(game, main_question):
+def engine(module):
     greeting()
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(main_question)
+    print(module.GAME_QUESTION)
     counter = 0
     while counter < 3:
-        game()
-        (question, correct_answer) = game()
+        (question, correct_answer) = module.generate_round()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
