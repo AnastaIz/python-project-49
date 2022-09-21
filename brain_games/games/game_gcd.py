ffbb4651c00a71ca62ed806bc0ec4bb0fa42d8c2
Anastasia Izmaylova <abulatova1992@gmail.com>
2022-09-21 18:58:49 +0300

    fix gcd function

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -7,7 +7,7 @@ GAME_QUESTION = "Find the greatest common divisor of given numbers."
 def generate_round():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
-    
+
     m = min(number1, number2)
 
     for i in range(1, m + 1):
